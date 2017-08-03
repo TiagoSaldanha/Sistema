81b8ae395f5d6417902cdcbc8a38fdff38e64150
TiagoSaldanha <tiagosaldanha2016@outlook.com>
2017-08-03 00:27:41 -0300

    Criação do arquivo gulpfile.js com a configuração do gulp para compilar o sass

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var watch = require('gulp-watch');
+
+// Task para o sass
+gulp.task('sass', function(){
+	return sass('sass/**/*.sass').pipe(gulp.dest('css'))
+
+});
+
+// Task para watch
+gulp.task('watch', function(){
+	gulp.watch('sass/**/*.sass', ['sass']);
+});
+
+// Task para o default
+gulp.task('default', ['sass', 'watch']);
